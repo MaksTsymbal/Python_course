e69072bc81a3b00ac76371acc186d112e2065fc5
MaksTsymbal <mcimbal659@gmail.com>
2023-04-06 10:55:57 +0300

    Lesson_11

---
+++ Lesson_11/dop_task.py
@@ -0,0 +1,18 @@
+# Задача 2. Робота з файлами через map.
+#
+# В директорії titanic_data ви можете знайти файл titanic_data.csv.
+#
+# Напишіть програму, що видаляє другу колонку з цієї таблиці та зберігає оновлену таблицю
+# у файл titanic_data_1.csv.
+#
+# Бажано використовувати map для якомога більшої долі операцій.
+#
+# Hint: ви можете зробити за допомогою map все, окрім власне роботи з файлами
+import csv
+with open('titanic_data.csv', 'r') as input_file, open('titanic_data_1.csv', 'w', newline='') as output_file:
+    reader = csv.reader(input_file)
+    writer = csv.writer(output_file)
+
+    for row in map(lambda x: [x[0], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11]], reader):
+        writer.writerow(row)
+
